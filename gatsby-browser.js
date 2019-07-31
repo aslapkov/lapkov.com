2cfd0e2d536ade3f417822e693a4fefa8953e248
Lapkov A. S <a.s.lapkov@gmail.com>
2019-07-31 17:43:58 +0300

    Add yandex metrika, gix favicon

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+exports.onRouteUpdate = function({ location }) {
+  if (
+    process.env.NODE_ENV === `production` &&
+    typeof window.yaCounter !== `undefined` &&
+    typeof window.yaCounter.hit === 'function'
+  ) {
+    window.yaCounter.hit((location || {}).pathname);
+  }
+};
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,3 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
-
 const React = require('react');
 const { renderToString } = require('react-dom/server');
 const { ServerStyleSheet, StyleSheetManager } = require('styled-components');
@@ -21,4 +15,56 @@ exports.replaceRenderer = ({
   );
   replaceBodyHTMLString(renderToString(<app />));
   setHeadComponents([sheet.getStyleElement()]);
+};
+
+exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
+  if (process.env.NODE_ENV === `production`) {
+    const useBeta = pluginOptions.version && pluginOptions.version === 2;
+    const versionSuffix = useBeta ? '2' : '';
+    const metrikaSrc = `https://mc.yandex.ru/metrika/${
+      useBeta ? 'tag' : 'watch'
+    }.js`;
+
+    return setPostBodyComponents([
+      <script
+        key={`metrika`}
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function (d, w, c) {
+                (w[c] = w[c] || []).push(function() {
+                    try {
+                        w.yaCounter = new Ya.Metrika${versionSuffix}({
+                            id:${pluginOptions.trackingId},
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true,
+                            webvisor:${pluginOptions.webvisor},
+                            trackHash:${pluginOptions.trackHash}
+                        });
+                    } catch(e) { }
+                });
+                var n = d.getElementsByTagName("script")[0],
+                    s = d.createElement("script"),
+                    f = function () { n.parentNode.insertBefore(s, n); };
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = "${metrikaSrc}";
+                if (w.opera == "[object Opera]") {
+                    d.addEventListener("DOMContentLoaded", f, false);
+                } else { f(); }
+            })(document, window, "yandex_metrika_callbacks${versionSuffix}");
+          `,
+        }}
+      />,
+      <noscript>
+        <div>
+          <img
+            src={`https://mc.yandex.ru/watch/${pluginOptions.trackingId}`}
+            style={{ position: 'absolute', left: '-9999px' }}
+            alt=""
+          />
+        </div>
+      </noscript>,
+    ]);
+  }
 };
