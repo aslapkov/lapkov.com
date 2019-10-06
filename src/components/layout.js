bd79d24356f3cc8bc835fb00fcb494eeb699e196
Lapkov A. S <a.s.lapkov@gmail.com>
2019-10-06 15:26:50 +0300

    Fix bugs

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import ImageBack from './imageBack';
-import NavBar from './navBar';
+import NavBar from './navbar';
 import Header from './header';
 import Footer from './footer';
 
