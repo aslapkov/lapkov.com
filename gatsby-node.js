d27745a32f4c9fa2d5e110d56ba40382a16d7784
Lapkov A. S <a.s.lapkov@gmail.com>
2019-10-17 11:04:41 +0300

    Fix ya mc tracking id

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
   const config = getConfig();
   if (stage.startsWith('develop') && config.resolve) {
