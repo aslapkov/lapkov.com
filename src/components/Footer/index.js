db491a8985827b805aa62ede5e281662aceb1b00
Lapkov A. S <a.s.lapkov@gmail.com>
2020-04-17 13:02:15 +0300

    Up deps, slowly moving away from styled component to css modules

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,41 +1,29 @@
 import React from 'react';
-
-import {
-  FooterWrap,
-  FooterLeft,
-  AvailPoint,
-  AvailTag,
-  FooterRigth,
-} from './footer.style';
+import styles from './Footer.module.css';
 
 let year = new Date().getFullYear();
 
-const Footer = () => (
-  <FooterWrap>
-    <FooterLeft>
-      <AvailPoint />
-      <AvailTag>
-        <span>Available for a full-time </span>
+export default () => (
+  <footer>
+    <div className={styles.left}>
+      <span className={styles.pointAnimation} />
+      <p className={styles.availTag}>
+        <span>Available for a full-time</span>
         <br />
-        <span>position </span>
+        <span>position</span>
+        {` `}
         <a href="mailto:email@lapkov.com">email@lapkov.com</a>
-      </AvailTag>
-    </FooterLeft>
-    <FooterRigth>
-      <div className="links">
+      </p>
+    </div>
+    <div className={styles.right}>
+      <div className={styles.links}>
         {year},{` `}
         <a href="https://github.com/aslapkov/lapkov.com">Open Source</a>
         <br />
-        Built with
-        {` `}
-        <a href="https://github.com/gatsbyjs/gatsby">Gatsby</a>
+        Built with <a href="https://github.com/gatsbyjs/gatsby">Gatsby</a>
         <br />
-        Deploy to
-        {` `}
-        <a href="https://www.netlify.com/docs/">Netlify</a>
+        Deploy to <a href="https://www.netlify.com/docs/">Netlify</a>
       </div>
-    </FooterRigth>
-  </FooterWrap>
+    </div>
+  </footer>
 );
-
-export default Footer;
