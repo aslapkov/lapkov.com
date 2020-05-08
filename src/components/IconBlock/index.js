6fd6104d6ea153ab77a299f3b2f5d98de466bdf4
Lapkov A. S <a.s.lapkov@gmail.com>
2020-05-08 23:01:03 +0300

    Fix seo, up deps

--- src/components/IconBlock/index.js
+++ src/components/IconBlock/index.js
@@ -1,27 +1,27 @@
 import React, { Fragment } from 'react';
-import { BlockRow, BlockDiv } from './iconBlock.style';
+import styles from './IconBlock.module.css';
 
 const IconBlock = ({ data }) => (
-  <BlockRow>
+  <div className={styles.blockRow}>
     {data.map((elem, i) => {
       const IconName = elem.iconName;
 
       return (
         <Fragment key={i}>
-          <BlockDiv>
+          <div className={styles.blockDiv}>
             <IconName
               height="100"
               width="100"
-              className="iconStyle"
+              className={styles.iconStyle}
               fill="#777"
             />
-            <span className="lab">{elem.label}</span>
-          </BlockDiv>
-          {i % 2 !== 0 && <div className="w100"></div>}
+            <span className={styles.lab}>{elem.label}</span>
+          </div>
+          {i % 2 !== 0 && <div className={styles.w100}></div>}
         </Fragment>
       );
     })}
-  </BlockRow>
+  </div>
 );
 
 export default IconBlock;
